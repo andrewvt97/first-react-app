b1f480254130562e3f3b84c0913fd35eb7cfb971
Andrew <andrewvt97@gmail.com>
2023-10-27 15:17:40 -0400

    Add list of my favorite foods

---
+++ src/Greetings.jsx
@@ -0,0 +1,22 @@
+function Greeting(){
+    return (
+    <div>
+        <h1>My Favorite Foods</h1>
+    <ol className="Food">
+        <li>Pizza </li>
+        <li>Quesadillas </li>
+        <li>Paneer </li>
+    </ol>
+    <svg >
+    <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+    </svg>
+    <form><input type="text"></input></form>
+
+
+    </div>
+    );
+    
+}
+
+
+export default Greeting;
